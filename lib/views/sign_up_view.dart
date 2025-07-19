b14dad236abca799d54c7ac9b589a1e3a39e2c3a
Youssef mahmoud eid <ym1977@fayoum.edu.eg>
2025-07-20 02:04:21 +0300

    add singlescroll view

--- lib/views/sign_up_view.dart
+++ lib/views/sign_up_view.dart
@@ -12,79 +12,78 @@ class SignUpView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.symmetric(horizontal: 18, vertical: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TitleText(text: 'Sign Up'),
-              SubTitle(text: 'create your account'),
-              SizedBox(height: 70),
-              TextFieldSection(),
-              SizedBox(height: 30),
+        child: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 18, vertical: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TitleText(text: 'Sign Up'),
+                SubTitle(text: 'create your account'),
+                SizedBox(height: 70),
+                TextFieldSection(),
+                SizedBox(height: 30),
 
-              ButtonWidget(
-                textcolor: Colors.white,
-                color: Color(0XFF9B28B2),
-                text: 'Sign Up',
-                onTap: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => LoginView()),
-                  );
-                },
-              ),
-              SizedBox(height: 15),
-              Text(
-                'Or',
-                style: TextStyle(
-                  fontSize: 17,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
+                ButtonWidget(
+                  textcolor: Colors.white,
+                  color: Color(0XFF9B28B2),
+                  text: 'Sign Up',
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => LoginView()),
+                    );
+                  },
                 ),
-              ),
-              SizedBox(height: 15),
-              ButtonWidget(
-                text: 'Sign in with Google',
-                textcolor: Color.fromARGB(255, 162, 85, 168),
-                border: Border.all(
-                  color: Colors.black, // Border color
-                  width: 1.5, // Border thickness
-                ),
-              ),
-              Spacer(),
-
-              Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Dont have an account ?',
-                    style: TextStyle(
-                      fontSize: 17,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400,
-                    ),
+                SizedBox(height: 15),
+                Text(
+                  'Or',
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => LoginView()),
-                      );
-                    },
-                    child: Text(
-                      'Login',
+                ),
+                SizedBox(height: 15),
+                ButtonWidget(
+                  text: 'Sign in with Google',
+                  textcolor: Color.fromARGB(255, 162, 85, 168),
+                  border: Border.all(
+                    color: Colors.black, // Border color
+                    width: 1.5, // Border thickness
+                  ),
+                ),
+                SizedBox(height: MediaQuery.sizeOf(context).height * .05),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Dont have an account ?',
                       style: TextStyle(
                         fontSize: 17,
-                        color: Color(0XFF9B28B2),
-                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Spacer(),
-            ],
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => LoginView()),
+                        );
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Color(0XFF9B28B2),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
